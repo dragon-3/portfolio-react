c0c3b331a81ae7de6e73b1301c3106040c89456a
Sean Narron <seannarron9@gmail.com>
2021-10-07 16:17:04 +0900

    Added content to about section & styling

--- src/HomeComponent.jsx
+++ src/HomeComponent.jsx
@@ -14,17 +14,45 @@ class HomeComponent extends React.Component {
                         <img src={profile} />
                     </div>
                 </div>
+
                 <div className="about">
-                    <h4>About:</h4>
-                    <p>I'm currently working as a Front End Engineer for an Asian company, mainly using programming languages such as Javascript and frameworks like React. Building and designing the User Interface has become one of my passions since we are so interconnected through the internet. I'm also working on projects like CRUD apps not only using Frontend tools, but also with some backend languages/frameworks. Check my Github account <a href="https://github.com/dragon-3">here</a>.   </p>
+                    <h3>About:</h3>
+                    <p>I'm currently working as a Front End Engineer for an Asian company, 
+                        mainly using programming languages such as Javascript and frameworks 
+                        like React. Building and designing the User Interface has become one 
+                        of my passions since we are so interconnected through the internet. 
+                        I'm also working on projects like CRUD apps not only using Frontend 
+                        tools, but also with some backend languages/frameworks. 
+                        Check my Github account <a href="https://github.com/dragon-3">here</a>.  
+                    </p>
+
+                    <p>Other project URLs: <br />
+                    <a href="https://seannarron.com/hokuriku/">https://seannarron.com/hokuriku/</a><br />
+                    <a href="https://yushirom.com/">https://yushirom.com/</a>
+                    </p>
                 </div>
+
                 <div className="skills">
-                    <h4>Skills:</h4>
-                    <p>I'm currently working as a Front End Engineer for an Asian company, mainly using programming languages such as Javascript and frameworks like React. Building and designing the User Interface has become one of my passions since we are so interconnected through the internet. </p>
+                    <h3>Skills:</h3>
+                    <p>・Able to create User Interfaces using <strong>Javascript</strong> and <strong>React</strong></p>
+                    <p>・Deep understanding of basic <strong>front-end technologies</strong></p>
+                    <p>・Experience in building <strong>SPAs</strong> with <strong>Restuful APIs</strong></p>
+                    <p>・Basics of <strong>Databases and SQL</strong></p>
                 </div>
-                <div className="about">
-                    <h4>About:</h4>
-                    <p>I'm currently working as a Front End Engineer for an Asian company, mainly using programming languages such as Javascript and frameworks like React. Building and designing the User Interface has become one of my passions since we are so interconnected through the internet. </p>
+
+                <hr />
+
+                <div className="contact">
+                    <h1>Interested in doing something together? </h1>
+                    <div className="contact-box">
+                        <h3><a href="mailto:seannarron9@gmail.com">Email Me ➡</a></h3>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div className="copyright">
+                    <h3>Copyright Sean Narron 2021</h3>
                 </div>
             </div>
             
