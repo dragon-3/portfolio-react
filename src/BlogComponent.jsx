9efdc072cce7f5b25bbfa2f198fbb69a440b668c
Sean Narron <seannarron9@gmail.com>
2021-10-07 16:48:23 +0900

    Created a blog component and styled it

--- src/BlogComponent.jsx
+++ src/BlogComponent.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import {withRouter} from 'react-router' 
+import { Link } from 'react-router-dom';
 
 
 class BlogComponent extends React.Component {
     render() {
         return (
-            <div>
-                blog
+            <div className="blog-component">
+
+                <div className="blog-intro">
+                    <h1>Blog: Thoughts on Life in general, and as a Front End Developer.</h1>
+                </div>
+
+                <hr />
+
+                <div className="blog-box">
+                    <div className="blog-content">
+                        <h3>Latest Blog Posts:</h3><br />
+                        <p>Oct 10, 2021</p>
+                        <h2>New Portfolio Website Out!</h2><br />
+                        <h4><Link to="/blog/1" style={{textDecoration: 'none'}}>Read More ➡</Link> </h4>
+                    </div>
+                    <hr />
+                </div>
+
             </div>
             
         )
