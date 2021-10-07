81229456e84e124262e97b549ffc92ca5b896f3e
Sean Narron <seannarron9@gmail.com>
2021-10-07 16:48:38 +0900

    Added a class name

--- src/HomeComponent.jsx
+++ src/HomeComponent.jsx
@@ -5,7 +5,7 @@ import profile from './portfolio-photo.JPG';
 class HomeComponent extends React.Component {
     render() {
         return (
-            <div>
+            <div className="home-component">
                 <div className="intro-box"> 
                     <div className="intro">
                         <h1>Hello, my name is Sean. I am a Junior Front End Developer. </h1>
