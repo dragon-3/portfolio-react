691ea589f8bdcf1e54f4978e0baf418d514bcf9e
Sean Narron <seannarron9@gmail.com>
2021-10-08 16:27:16 +0900

    Added personal info section in resume component

--- src/Components/ResumeComponent.jsx
+++ src/Components/ResumeComponent.jsx
@@ -10,6 +10,69 @@ class ResumeComponent extends React.Component {
                     <h1>Sean Narron</h1>
                     <h2>Front End Developer</h2>
                 </div>
+                <div className="resume-box">
+                    <div className="resume-intro">
+                        <p>Junior Front End Developer with 2 years experience in Web Development.</p>
+                    </div>
+                    <div className="resume-info">
+                        <h4>Personal Info</h4>
+                        <hr />
+
+                        <h5>Email</h5>
+                        <p>seannarron9@gmail.com</p>
+
+                        <br />
+
+                        <h4>Skills</h4>
+                        <hr />
+
+                        <p>Willingness to learn</p>
+                        <div className="right-dots">
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                        </div>
+
+                        <p>Team player</p>
+                        <div className="right-dots">
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot" style={{backgroundColor:"rgb(218, 214, 214)"}}></p>
+                        </div>
+                        
+                        
+                        <p>Sincerity</p>
+                        <div className="right-dots">
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                        </div>
+                        <p>Basic frontend stack (HTML, CSS, JS, React)</p>
+                        <div className="right-dots">
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot" style={{backgroundColor:"rgb(218, 214, 214)"}}></p>
+                        </div>
+                        <p>Love for React development</p>
+                        <div className="right-dots">
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot"></p>
+                            <p className="dot" style={{backgroundColor:"rgb(65, 64, 64)"}}></p>
+                        </div>
+
+                    </div>
+                </div>
+                
             </div>
             
         )
