dbbf7be45c84cb83ac7cb0f9b78943893e439c31
Sean Narron <seannarron9@gmail.com>
2021-10-13 17:29:47 +0900

    Created responsive navbar menu

--- src/Components/HeaderComponent.jsx
+++ src/Components/HeaderComponent.jsx
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+import { useState } from 'react';
+import '../Navbar.css'
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 
-class HeaderComponent extends React.Component {
-    render() {
+
+
+function HeaderComponent() {
+
+    const [toggleMenu, setToggleMenu] = useState(false)
+
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
+    useEffect(() => {
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        }
+
+        window.addEventListener('resize', changeWidth)
+
+        return() => {
+            window.removeEventListener('resize', changeWidth)
+        }
+    }, [])
+
         return (
-            <div>
+            <div >
+
+                <nav>
+                    {(toggleMenu || screenWidth > 500) && (
+                        <ul className="list">
+                        <Link to="/" style={{textDecoration: 'none'}}><li className="items">Home</li></Link>              
+                        <Link to="/blog" style={{textDecoration: 'none'}}><li className="items">Blog</li></Link>
+                        <Link to="/resume" style={{textDecoration: 'none'}}><li className="items">Resume</li></Link>
+                        </ul>
+                    )}
+                    
+                    <button onClick={toggleNav} className="btn">Menu</button>
                 
-                <div className="navbar">
-                <Link to="/" style={{textDecoration: 'none'}}>Home</Link>
-                <Link to="/blog" style={{textDecoration: 'none'}}>Blog</Link>
-                <Link to="/resume" style={{textDecoration: 'none'}}>Resume</Link>
-                </div>
+                </nav>
+                
+                
+
+                
+                
             </div>
             
         )
-    }
 }
 
 export default HeaderComponent
